add39c5150a8c1df6aad9bcfa325ae7bc2d1b9f8
clubby789 <jamie@hill-daniel.co.uk>
2024-12-01 08:58:05 +0000

    day1 part1: Preallocate and effiicently parse

--- src/day1.rs
+++ src/day1.rs
@@ -2,18 +2,19 @@ use rustc_hash::FxHashMap;
 
 use crate::util::FastParse;
 
-// 26.8us
-pub fn part1(input: &str) -> u64 {
-    let (mut left, mut right): (Vec<_>, Vec<_>) = input
-        .lines()
-        .map(|l| {
-            let (l, r) = l.split_once(char::is_whitespace).unwrap();
-            (
-                u64::fast_parse(l).unwrap().0,
-                u64::fast_parse(r.trim_ascii_start()).unwrap().0,
-            )
-        })
-        .unzip();
+// 14.9us
+pub fn part1(mut input: &str) -> u64 {
+    let lines = input.len() / memchr::memchr(b'\n', input.as_bytes()).unwrap();
+    let mut left = Vec::with_capacity(lines);
+    let mut right = Vec::with_capacity(lines);
+    while !input.is_empty() {
+        let (num1, len) = u64::fast_parse(input).unwrap();
+        input = &input[len..].trim_ascii_start();
+        left.push(num1);
+        let (num2, len) = u64::fast_parse(input).unwrap();
+        input = &input[len + 1..];
+        right.push(num2);
+    }
     left.sort_unstable();
     right.sort_unstable();
     left.iter().zip(&right).map(|(l, r)| l.abs_diff(*r)).sum()
