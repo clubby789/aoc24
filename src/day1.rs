44a867c5587bfc55dee6f41ec5d8e43f5b03fb99
clubby789 <jamie@hill-daniel.co.uk>
2024-12-01 08:43:10 +0000

    day1 part2: Use a running hashmap to speed up

--- src/day1.rs
+++ src/day1.rs
@@ -1,6 +1,11 @@
 const INPUT: &str = include_str!("inputs/1.txt");
+const _: () = assert!(INPUT.as_bytes()[INPUT.len() - 1] == b'\n');
+
+use rustc_hash::FxHashMap;
+
 use crate::util::FastParse;
 
+// 25.2us
 pub fn part1() -> u64 {
     let (mut left, mut right): (Vec<_>, Vec<_>) = INPUT
         .lines()
@@ -17,18 +22,25 @@ pub fn part1() -> u64 {
     left.iter().zip(&right).map(|(l, r)| l.abs_diff(*r)).sum()
 }
 
+// 20.11us
 pub fn part2() -> u64 {
-    let (left, right): (Vec<_>, Vec<_>) = INPUT
-        .lines()
-        .map(|l| {
-            let (l, r) = l.split_once(char::is_whitespace).unwrap();
-            (
-                u64::fast_parse(l).unwrap().0,
-                u64::fast_parse(r.trim_ascii_start()).unwrap().0,
-            )
-        })
-        .unzip();
-    left.iter()
-        .map(|&val| right.iter().filter(|&&v| v == val).count() as u64 * val)
+    #[derive(Default)]
+    struct Num {
+        count: u64,
+        appeared_left: bool,
+    }
+    let mut nums: FxHashMap<u64, Num> = FxHashMap::default();
+    let mut input = INPUT;
+    while !input.is_empty() {
+        let (num1, len) = u64::fast_parse(input).unwrap();
+        input = &input[len..].trim_ascii_start();
+        nums.entry(num1).or_default().appeared_left = true;
+        let (num2, len) = u64::fast_parse(input).unwrap();
+        input = &input[len + 1..];
+        nums.entry(num2).or_default().count += 1;
+    }
+    nums.iter()
+        .filter(|(_, num)| num.appeared_left)
+        .map(|(val, num)| val * num.count)
         .sum()
 }
