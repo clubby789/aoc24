0b6fdb9397419fcb7ff1d1078fbbc959827c7075
clubby789 <jamie@hill-daniel.co.uk>
2024-12-02 16:36:36 +0000

    day2: refactor and use fast parser for part 2

--- src/day2.rs
+++ src/day2.rs
@@ -2,11 +2,50 @@ use std::cmp::Ordering;
 
 use crate::util::FastParse;
 
-// 7.5us
+// 8.5us
 pub fn part1(input: &str) -> u64 {
     let mut input = input.as_bytes();
     let mut count = 0;
 
+    while !input.is_empty() {
+        let result;
+        (input, result) = check_line_valid(input);
+        if result.is_ok() {
+            count += 1;
+        }
+    }
+    count
+}
+
+// 30.2us
+pub fn part2(input: &str) -> u64 {
+    let mut input = input.as_bytes();
+    let mut count = 0;
+
+    while !input.is_empty() {
+        let (next_input, result) = check_line_valid(input);
+        match result {
+            Ok(_) => count += 1,
+            Err(idx) => {
+                // SAFETY: `input` came from a UTF-8 string. `check_line_valid` splits off only ASCII characters
+                let (line, _) = unsafe {
+                    std::str::from_utf8_unchecked(input)
+                        .split_once('\n')
+                        .unwrap()
+                };
+                let mut numbers = Vec::with_capacity(line.len() / 2);
+                numbers.extend(line.split_ascii_whitespace().map(u64::fast_parse_unchecked));
+                if check_sequence_valid_damped(numbers, idx) {
+                    count += 1;
+                }
+            }
+        }
+        input = next_input;
+    }
+    count
+}
+
+fn check_line_valid(mut input: &[u8]) -> (&[u8], Result<(), usize>) {
     #[derive(Default)]
     enum State {
         #[default]
@@ -15,70 +54,42 @@ pub fn part1(input: &str) -> u64 {
         InProgress(Ordering, u8),
     }
 
-    while !input.is_empty() {
-        let mut state = State::default();
-        let ok;
-        (input, ok) = for_each_num_on_line(input, |num| match state {
-            State::New => {
-                state = State::First(num);
+    let mut state = State::default();
+    let mut checked = 0;
+    let ok;
+    (input, ok) = for_each_num_on_line(input, |num| match state {
+        State::New => {
+            state = State::First(num);
+            true
+        }
+        State::First(prev) => {
+            if matches!(num.abs_diff(prev), 1..=3) {
+                state = State::InProgress(num.cmp(&prev), num);
+                checked += 1;
                 true
+            } else {
+                false
             }
-            State::First(prev) => {
-                if matches!(num.abs_diff(prev), 1..=3) {
-                    state = State::InProgress(num.cmp(&prev), num);
-                    true
-                } else {
-                    false
-                }
-            }
-            State::InProgress(direction, ref mut prev) => {
-                if matches!(num.abs_diff(*prev), 1..=3) && num.cmp(prev) == direction {
-                    *prev = num;
-                    true
-                } else {
-                    false
-                }
-            }
-        });
-
-        if ok {
-            count += 1;
-        } else {
-            input = &input[memchr::memchr(b'\n', input).unwrap() + 1..];
         }
-    }
-    count
-}
-
-// 40.0us
-pub fn part2(input: &str) -> u64 {
-    input
-        .lines()
-        .filter(|line| {
-            let mut nums = Vec::with_capacity(line.len() / 2);
-            nums.extend(line.split_ascii_whitespace().map(u64::fast_parse_unchecked));
-            match check_sequence_valid(&nums) {
-                Ok(_) => true,
-                Err(idx) => {
-                    // Outline as rarely called
-                    check_sequence_valid_damped(nums, idx)
-                }
+        State::InProgress(direction, ref mut prev) => {
+            if matches!(num.abs_diff(*prev), 1..=3) && num.cmp(prev) == direction {
+                *prev = num;
+                checked += 1;
+                true
+            } else {
+                false
             }
-        })
-        .count() as _
-}
-
-fn check_sequence_valid(nums: &[u64]) -> Result<(), usize> {
-    let direction = nums[1].cmp(&nums[0]);
-    for i in 0..nums.len() - 1 {
-        let n1 = nums[i];
-        let n2 = nums[i + 1];
-        match n2.abs_diff(n1) {
-            1..=3 if n2.cmp(&n1) == direction => continue,
-            _ => return Err(i),
         }
+    });
+
+    if ok {
+        (input, Ok(()))
+    } else {
+        (
+            &input[memchr::memchr(b'\n', input).unwrap() + 1..],
+            Err(checked),
+        )
     }
-    Ok(())
 }
 
 #[cold]
@@ -94,6 +105,19 @@ fn check_sequence_valid_damped(mut nums: Vec<u64>, idx: usize) -> bool {
     false
 }
 
+fn check_sequence_valid(nums: &[u64]) -> Result<(), usize> {
+    let direction = nums[1].cmp(&nums[0]);
+    for i in 0..nums.len() - 1 {
+        let n1 = nums[i];
+        let n2 = nums[i + 1];
+        match n2.abs_diff(n1) {
+            1..=3 if n2.cmp(&n1) == direction => continue,
+            _ => return Err(i),
+        }
+    }
+    Ok(())
+}
+
 fn for_each_num_on_line<F: FnMut(u8) -> bool>(mut input: &[u8], mut f: F) -> (&[u8], bool) {
     loop {
         let (num, last, rest) = match input {
@@ -104,13 +128,13 @@ fn for_each_num_on_line<F: FnMut(u8) -> bool>(mut input: &[u8], mut f: F) -> (&[
             _ => unreachable!("{:?}", input),
         };
 
-        input = rest;
         if f(num) {
             if last {
-                return (input, true);
+                return (rest, true);
             }
         } else {
             return (input, false);
         }
+        input = rest;
     }
 }
