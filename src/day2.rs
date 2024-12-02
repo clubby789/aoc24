9049361a2c3e377dc87a932c52a3387e3a425369
clubby789 <jamie@hill-daniel.co.uk>
2024-12-02 09:28:08 +0000

    day2 part2: use failing index to inform skips

--- src/day2.rs
+++ src/day2.rs
@@ -1,50 +1,52 @@
 use crate::util::FastParse;
 
-// 31.4us
+// 32.8us
 pub fn part1(input: &str) -> u64 {
     input
         .lines()
         .filter(|line| {
             let mut nums = Vec::with_capacity(line.len() / 2);
             nums.extend(line.split_ascii_whitespace().map(u64::fast_parse_unchecked));
-            check_sequence_valid(&nums)
+            check_sequence_valid(&nums).is_ok()
         })
         .count() as _
 }
 
-// 56.0us
+// 45.0us
 pub fn part2(input: &str) -> u64 {
     input
         .lines()
         .filter(|line| {
             let mut nums = Vec::with_capacity(line.len() / 2);
             nums.extend(line.split_ascii_whitespace().map(u64::fast_parse_unchecked));
-            if check_sequence_valid(&nums) {
-                true
-            } else {
-                // Push and pop from the same vec to retain the storage
-                let mut check = nums.clone();
-                for i in 0..nums.len() {
-                    let old = check.remove(i);
-                    if check_sequence_valid(&check) {
-                        return true;
+            match check_sequence_valid(&nums) {
+                Ok(_) => true,
+                Err(idx) => {
+                    // Push and pop from the same vec to retain the storage
+                    let mut check = nums.clone();
+                    for i in idx.saturating_sub(2)..(idx + 2).min(nums.len()) {
+                        let old = check.remove(i);
+                        if check_sequence_valid(&check).is_ok() {
+                            return true;
+                        }
+                        check.insert(i, old);
                     }
-                    check.insert(i, old);
+                    false
                 }
-                false
             }
         })
         .count() as _
 }
 
-fn check_sequence_valid(nums: &[u64]) -> bool {
+fn check_sequence_valid(nums: &[u64]) -> Result<(), usize> {
     let direction = nums[1].cmp(&nums[0]);
-    nums.windows(2).all(move |window| {
-        let n1 = window[0];
-        let n2 = window[1];
+    for i in 0..nums.len() - 1 {
+        let n1 = nums[i];
+        let n2 = nums[i + 1];
         match n2.abs_diff(n1) {
-            1..=3 if n2.cmp(&n1) == direction => true,
-            _ => false,
+            1..=3 if n2.cmp(&n1) == direction => continue,
+            _ => return Err(i),
         }
-    })
+    }
+    Ok(())
 }
