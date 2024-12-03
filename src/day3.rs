81b874cb6ef13f90940696d153e70b65d7685561
clubby789 <jamie@hill-daniel.co.uk>
2024-12-03 16:39:18 +0000

    day 3: don't return option from parser and search with 1 byte

--- src/day3.rs
+++ src/day3.rs
@@ -1,58 +1,76 @@
 use memchr::memmem;
 
-// 5.5us
+// 5.1us
 pub fn part1(input: &str) -> u64 {
     let mut input = input.as_bytes();
     let mut total = 0;
-    let finder = memmem::Finder::new(b"mul(");
 
-    while let Some(start) = finder.find(input) {
-        input = &input[start + 4..];
-        if let Some((x, y, rest)) = parse_mul_body(input) {
-            total += x * y;
-            input = rest;
+    while let Some(start) = memchr::memchr(b'm', input) {
+        input = &input[start + 1..];
+        match input {
+            [b'u', b'l', b'(', rest @ ..] => {
+                let val;
+                (val, input) = parse_mul_body(rest);
+                total += val;
+            }
+            _ => continue,
         }
     }
     total
 }
 
-// 12.1us
+// 12.0us
 pub fn part2(input: &str) -> u64 {
     let mut input = input.as_bytes();
     let mut total = 0;
     let mut enabled = true;
-    let mul_finder = memmem::Finder::new(b"mul(");
     let enable_finder = memmem::FinderRev::new(b"do()");
     let disable_finder = memmem::FinderRev::new(b"don't()");
 
-    while let Some(start) = mul_finder.find(input) {
+    while let Some(start) = memchr::memchr(b'm', input) {
         let to_check = &input[..start];
-        let last_enable = enable_finder.rfind(to_check);
-        let last_disable = disable_finder.rfind(to_check);
-        if enabled {
-            // If there is a disable, and there is no enable OR the last enable is before the last disable:
-            if last_disable.is_some_and(|last_disable_pos| {
-                last_enable.is_none_or(|last_enable_pos| last_enable_pos < last_disable_pos)
-            }) {
-                enabled = false;
-            }
-        } else {
-            // If there is a enable, and there is no disable OR the last disable is before the last enable:
-            if last_enable.is_some_and(|last_enable_pos| {
-                last_disable.is_none_or(|last_disable_pos| last_disable_pos < last_enable_pos)
-            }) {
-                enabled = true;
-            }
-        }
-        input = &input[start + 4..];
-        if enabled {
-            if let Some((x, y, rest)) = parse_mul_body(input) {
-                total += x * y;
+        let (val, new_input) = match &input[start + 1..] {
+            [b'u', b'l', b'(', rest @ ..] => parse_mul_body(rest),
+            rest => {
+                enabled = enabled_after_block(enabled, &enable_finder, &disable_finder, &to_check);
                 input = rest;
+                continue;
             }
+        };
+        enabled = enabled_after_block(enabled, &enable_finder, &disable_finder, &to_check);
+        input = new_input;
+        if enabled {
+            total += val;
         }
     }
     total
+}
+
+#[inline]
+fn enabled_after_block(
+    mut enabled: bool,
+    enable_finder: &memmem::FinderRev,
+    disable_finder: &memmem::FinderRev,
+    block: &[u8],
+) -> bool {
+    let last_enable = enable_finder.rfind(block);
+    let last_disable = disable_finder.rfind(block);
+    if enabled {
+        // If there is a disable, and there is no enable OR the last enable is before the last disable:
+        if last_disable.is_some_and(|last_disable_pos| {
+            last_enable.is_none_or(|last_enable_pos| last_enable_pos < last_disable_pos)
+        }) {
+            enabled = false;
+        }
+    } else {
+        // If there is a enable, and there is no disable OR the last disable is before the last enable:
+        if last_enable.is_some_and(|last_enable_pos| {
+            last_disable.is_none_or(|last_disable_pos| last_disable_pos < last_enable_pos)
+        }) {
+            enabled = true;
+        }
+    }
+    enabled
 }
 
 macro_rules! parse_ascii {
@@ -67,9 +85,8 @@ macro_rules! parse_ascii {
     };
 }
 
-// Given bytes after a `mul(`, return the X and Y values (if valid) and the bytes after the mul
-fn parse_mul_body(input: &[u8]) -> Option<(u64, u64, &[u8])> {
-    let (x, y, rest) = match input {
+fn parse_mul_body(input: &[u8]) -> (u64, &[u8]) {
+    let (val, rest) = match input {
         // (XXX,YYY)
         [
             x1 @ b'0'..=b'9',
@@ -81,7 +98,7 @@ fn parse_mul_body(input: &[u8]) -> Option<(u64, u64, &[u8])> {
             y3 @ b'0'..=b'9',
             b')',
             rest @ ..,
-        ] => (parse_ascii!(x1, x2, x3), parse_ascii!(y1, y2, y3), rest),
+        ] => (parse_ascii!(x1, x2, x3) * parse_ascii!(y1, y2, y3), rest),
         // (XXX,YY)
         [
             x1 @ b'0'..=b'9',
@@ -92,7 +109,7 @@ fn parse_mul_body(input: &[u8]) -> Option<(u64, u64, &[u8])> {
             y2 @ b'0'..=b'9',
             b')',
             rest @ ..,
-        ] => (parse_ascii!(x1, x2, x3), parse_ascii!(y1, y2), rest),
+        ] => (parse_ascii!(x1, x2, x3) * parse_ascii!(y1, y2), rest),
         // (XXX,Y)
         [
             x1 @ b'0'..=b'9',
@@ -102,7 +119,7 @@ fn parse_mul_body(input: &[u8]) -> Option<(u64, u64, &[u8])> {
             y1 @ b'0'..=b'9',
             b')',
             rest @ ..,
-        ] => (parse_ascii!(x1, x2, x3), parse_ascii!(y1), rest),
+        ] => (parse_ascii!(x1, x2, x3) * parse_ascii!(y1), rest),
         // (XX,YYY)
         [
             x1 @ b'0'..=b'9',
@@ -113,7 +130,7 @@ fn parse_mul_body(input: &[u8]) -> Option<(u64, u64, &[u8])> {
             y3 @ b'0'..=b'9',
             b')',
             rest @ ..,
-        ] => (parse_ascii!(x1, x2), parse_ascii!(y1, y2, y3), rest),
+        ] => (parse_ascii!(x1, x2) * parse_ascii!(y1, y2, y3), rest),
         // (XX,YY)
         [
             x1 @ b'0'..=b'9',
@@ -123,7 +140,7 @@ fn parse_mul_body(input: &[u8]) -> Option<(u64, u64, &[u8])> {
             y2 @ b'0'..=b'9',
             b')',
             rest @ ..,
-        ] => (parse_ascii!(x1, x2), parse_ascii!(y1, y2), rest),
+        ] => (parse_ascii!(x1, x2) * parse_ascii!(y1, y2), rest),
         // (XX,Y)
         [
             x1 @ b'0'..=b'9',
@@ -132,7 +149,7 @@ fn parse_mul_body(input: &[u8]) -> Option<(u64, u64, &[u8])> {
             y1 @ b'0'..=b'9',
             b')',
             rest @ ..,
-        ] => (parse_ascii!(x1, x2), parse_ascii!(y1), rest),
+        ] => (parse_ascii!(x1, x2) * parse_ascii!(y1), rest),
         // (X,YYY)
         [
             x1 @ b'0'..=b'9',
@@ -142,7 +159,7 @@ fn parse_mul_body(input: &[u8]) -> Option<(u64, u64, &[u8])> {
             y3 @ b'0'..=b'9',
             b')',
             rest @ ..,
-        ] => (parse_ascii!(x1), parse_ascii!(y1, y2, y3), rest),
+        ] => (parse_ascii!(x1) * parse_ascii!(y1, y2, y3), rest),
         // (X,YY)
         [
             x1 @ b'0'..=b'9',
@@ -151,12 +168,12 @@ fn parse_mul_body(input: &[u8]) -> Option<(u64, u64, &[u8])> {
             y2 @ b'0'..=b'9',
             b')',
             rest @ ..,
-        ] => (parse_ascii!(x1), parse_ascii!(y1, y2), rest),
+        ] => (parse_ascii!(x1) * parse_ascii!(y1, y2), rest),
         // (X,Y)
         [x1 @ b'0'..=b'9', b',', y1 @ b'0'..=b'9', b')', rest @ ..] => {
-            (parse_ascii!(x1), parse_ascii!(y1), rest)
+            (parse_ascii!(x1) * parse_ascii!(y1), rest)
         }
-        _ => return None,
+        _ => return (0, input),
     };
-    Some((x, y, rest))
+    (val, rest)
 }
