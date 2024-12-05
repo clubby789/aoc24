a2b1585200a7dfece38e867d2dda55a62555edf9
clubby789 <jamie@hill-daniel.co.uk>
2024-12-05 11:34:01 +0000

    Make running other days easier

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use advent_of_code::FUNCS;
 
 fn main() {
-    let (f1, f2) = FUNCS.last().unwrap();
-    let input = std::fs::read_to_string(format!("inputs/{}.txt", FUNCS.len())).unwrap();
+    let day = FUNCS.len();
+    let (f1, f2) = FUNCS[day - 1];
+    let input = std::fs::read_to_string(format!("inputs/{}.txt", day)).unwrap();
     println!("Part 1 - {}", f1(&input));
     println!("Part 2 - {}", f2(&input));
 }
