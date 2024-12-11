d66e5887d6261abc8b093f5c8cb0ecb0816a6741
clubby789 <jamie@hill-daniel.co.uk>
2024-12-11 00:41:16 +0000

    day9 part2: Reimplement using segments

--- src/day9.rs
+++ src/day9.rs
@@ -1,8 +1,8 @@
-use std::{fmt::Write, usize};
+use std::{fmt::Write, num::NonZeroU64, usize};
 
 // 228.7us
 pub fn part1(input: &str) -> u64 {
-    let mut disk = parse_disk(input);
+    let mut disk = parse_disk_blocks(input);
     let mut start = 0;
     let mut end = disk.len() - 1;
     while start < end {
@@ -20,82 +20,42 @@ pub fn part1(input: &str) -> u64 {
     disk_checksum_contiguous(&disk)
 }
 
-// 81.5ms
+// 16.1ms
 pub fn part2(input: &str) -> u64 {
-    let mut disk = parse_disk(input);
-    let mut end: usize = disk.len() - 1;
-    let mut last_tried_id = usize::MAX;
+    let mut disk = parse_disk_segments(input);
+    let mut last_block = u64::MAX;
+    let mut back = disk.len() - 1;
 
     'outer: loop {
-        while disk[end] == Block::Empty {
-            end -= 1;
-        }
-        let file_end = end;
-        let mut file_start = end;
-        while disk[file_start - 1] == disk[file_end] {
-            file_start -= 1;
-            if file_start == 0 {
+        while matches!(disk[back], Segment::Empty { .. }) {
+            let Some(nb) = back.checked_sub(1) else {
                 break 'outer;
-            }
+            };
+            back = nb;
         }
-        match disk[file_start] {
-            Block::File { id } if id >= last_tried_id => {
-                end = file_start - 1;
-                continue;
-            }
-            Block::File { id } if id == 0 => {
-                break;
-            }
-            Block::File { id } => last_tried_id = id,
-            Block::Empty => {
-                #[cfg(debug_assertions)]
-                unreachable!();
-            }
-        }
-        debug_assert!(file_end >= file_start);
-        let file_len = file_end - file_start;
-
-        let mut empty_start = 0;
-        let mut empty_end = 0;
-        let mut ok = false;
-        'find_empty: while empty_start < file_start {
-            // Find empty block
-            while disk[empty_start] != Block::Empty {
-                empty_start += 1;
-            }
-            if empty_start >= file_start {
-                break;
-            }
-            empty_end = empty_start;
-            while disk[empty_end + 1] == Block::Empty {
-                empty_end += 1;
-                if empty_end + 1 >= disk.len() {
-                    break 'find_empty;
-                }
-            }
-            if empty_end - empty_start < file_len {
-                empty_start = empty_end + 1;
-            } else {
-                ok = true;
-                break;
-            }
-        }
-        if ok {
-            // Found enough free space
-            debug_assert!(empty_end >= empty_start);
-            debug_assert!(file_start > empty_end);
-            for (empty, file) in (empty_start..=empty_end).zip(file_start..=file_end) {
-                disk.swap(empty, file);
-            }
+        let Segment::File { id, .. } = disk[back] else {
+            unreachable!()
+        };
+        if id >= last_block {
+            back -= 1;
+            continue;
         } else {
-            end = file_start - 1;
+            last_block = id;
+        }
+
+        // We only need to decrement `back` if new space wasn't inserted
+        if !disk.move_file_earlier(back) {
+            let Some(nb) = back.checked_sub(1) else {
+                break;
+            };
+            back = nb;
         }
     }
 
-    disk_checksum(&disk)
+    disk.checksum()
 }
 
-fn parse_disk(input: &str) -> Vec<Block> {
+fn parse_disk_blocks(input: &str) -> Vec<Block> {
     let mut disk = Vec::with_capacity(input.len() * 9);
     for (i, b) in input.trim_ascii_end().bytes().enumerate() {
         let block = if i & 1 == 0 {
@@ -111,6 +71,128 @@ fn parse_disk(input: &str) -> Vec<Block> {
     disk
 }
 
+#[derive(Clone, Copy)]
+enum Segment {
+    File { id: u64, size: NonZeroU64 },
+    Empty { size: NonZeroU64 },
+}
+
+impl Segment {
+    #[must_use]
+    pub fn insert_file(&mut self, id: u64, file_size: NonZeroU64) -> Option<Self> {
+        let Segment::Empty { size } = self else {
+            panic!("insert into file");
+        };
+
+        let Some(remaining) = size.get().checked_sub(file_size.get()) else {
+            panic!("insert into too small space");
+        };
+        *self = Segment::File {
+            id,
+            size: file_size,
+        };
+        NonZeroU64::new(remaining).map(|size| Segment::Empty { size })
+    }
+}
+
+impl std::fmt::Debug for Segment {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            Segment::File { id, size } => {
+                for _ in 0..size.get() {
+                    write!(f, "{id}")?
+                }
+                Ok(())
+            }
+            Segment::Empty { size } => {
+                for _ in 0..size.get() {
+                    write!(f, ".")?
+                }
+                Ok(())
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct SegmentDisk {
+    segments: Vec<Segment>,
+}
+
+impl SegmentDisk {
+    pub fn len(&self) -> usize {
+        self.segments.len()
+    }
+
+    // Returns true if new space was inserted
+    pub fn move_file_earlier(&mut self, file_idx: usize) -> bool {
+        let Segment::File { id, size } = self[file_idx] else {
+            panic!("non-file passed to `move_file_earlier`")
+        };
+        let Some(space) = self
+            .segments
+            .iter()
+            .take(file_idx)
+            .position(|seg| match seg {
+                Segment::Empty { size: empty_size } if *empty_size >= size => true,
+                _ => false,
+            })
+        else {
+            return false;
+        };
+
+        self.segments[file_idx] = Segment::Empty { size };
+        if let Some(new_space) = self.segments[space].insert_file(id, size) {
+            self.segments.insert(space + 1, new_space);
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn checksum(&self) -> u64 {
+        self.segments
+            .iter()
+            .fold((0, 0), |(total, block_pos), seg| {
+                match seg {
+                    Segment::File { id, size } => {
+                        let size = size.get();
+                        let val = id * (block_pos * size + ((size - 1) * size) / 2);
+                        (total + val, block_pos + size)
+                    }
+                    Segment::Empty { size } => (total, block_pos + size.get()),
+                }
+            })
+            .0
+    }
+}
+
+impl std::ops::Index<usize> for SegmentDisk {
+    type Output = Segment;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.segments[index]
+    }
+}
+
+fn parse_disk_segments(input: &str) -> SegmentDisk {
+    let mut segments = Vec::with_capacity(input.len());
+    for (i, b) in input.trim_ascii_end().bytes().enumerate() {
+        let Some(size) = NonZeroU64::new((b - b'0') as u64) else {
+            continue;
+        };
+        if i & 1 == 0 {
+            segments.push(Segment::File {
+                id: i as u64 / 2,
+                size,
+            });
+        } else {
+            segments.push(Segment::Empty { size });
+        };
+    }
+    SegmentDisk { segments }
+}
+
 fn disk_checksum_contiguous(disk: &[Block]) -> u64 {
     disk.iter()
         .enumerate()
@@ -120,19 +202,6 @@ fn disk_checksum_contiguous(disk: &[Block]) -> u64 {
                 unreachable!()
             };
             (pos * id) as u64
-        })
-        .sum()
-}
-
-fn disk_checksum(disk: &[Block]) -> u64 {
-    disk.iter()
-        .enumerate()
-        .map(|(pos, entry)| {
-            if let Block::File { id } = *entry {
-                (pos * id) as u64
-            } else {
-                0
-            }
         })
         .sum()
 }
